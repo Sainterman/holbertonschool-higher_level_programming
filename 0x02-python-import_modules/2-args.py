d9d68d49ed0d93daf7e4d854e91adf81ea4cc9a1
Santiago Castillo <sancastillog@gmail.com>
2020-05-01 02:57:32 +0000

    pushing task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv) - 1
+    if args is 0:
+        print("{:d} arguments.".format(args))
+    elif args is 1:
+        print("{:d} argument".format(args))
+        print("1: {:s}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(args))
+        for i in range(1, args + 1):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
