3f1538855b8e52956f2dd58089e2b8ae584c5aa1
Santiago Castillo <sancastillog@gmail.com>
2020-05-07 23:58:35 +0000

    defining empty conditio

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     residues = []
+    uniq = 0
     if my_list:
-        uniq = 0
         for item in my_list:
             if item not in residues:
+                uniq += item
                 residues.append(item)
-
-        for lonelies in residues:
-            uniq += lonelies
         return uniq
