1b3d5811f00621f96a2af573ae8eebf2e3804aad
Santiago Castillo <sancastillog@gmail.com>
2020-05-08 22:47:43 +0000

    pushing task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    romans = dict([("I", 1), ("V", 5), ("X", 10), ("L", 50), ("C", 100),
+                  ("D", 500), ("M", 1000)])
+    number = 0
+    ultimo = 0
+    for i in roman_string:
+        for roman in romans:
+            if i is roman:
+                actual = romans[roman]
+                if ultimo is 0 or ultimo >= actual:
+                    number += actual
+                elif ultimo < actual:
+                    number += actual - (ultimo * 2)
+                ultimo = actual
+    return number
