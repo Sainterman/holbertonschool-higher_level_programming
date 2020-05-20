f04bd8d75a973518aa09ff51bb912e84d611d85e
Santiago Castillo <sancastillog@gmail.com>
2020-05-20 02:12:01 +0000

    pushint task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Define a square class with size """
+
+
+class Square:
+    """ Square with size """
+    __size = 0
+
+    def __init__(self, size):
+        """ initialize a square with size """
+        self.__size = size
