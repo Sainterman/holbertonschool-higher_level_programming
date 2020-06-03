17c272993b83a9acd9acf142d800d8a8ad5ec791
Santiago Castillo <sancastillog@gmail.com>
2020-06-03 23:42:18 +0000

    iterating

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,8 +6,8 @@ def read_lines(filename="", nb_lines=0):
     """ open, check amount of lines, read and print """
     with open(filename, encoding='utf-8') as f:
         if nb_lines <= 0:
-            text = f.read()
-            print(text)
+            for i in f:
+                print(i, end="")
         else:
             for i in range(nb_lines):
                 print(f.readline(), end="")
