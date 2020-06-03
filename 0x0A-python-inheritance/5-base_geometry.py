517704926bf74d4376f7ae9c4013d783ce324a84
Santiago Castillo <sancastillog@gmail.com>
2020-06-03 01:24:54 +0000

    pushing task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" module describes class BaseGeometry """
+
+
+class BaseGeometry():
+    """ empty class """
+
+    def __init__(self):
+        """ nothing happens yet """
+
+        pass
