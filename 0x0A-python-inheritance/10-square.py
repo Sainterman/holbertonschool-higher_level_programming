7e9ab71c6fd92ba5c326f7a53a2af30e48af03b2
Santiago Castillo <sancastillog@gmail.com>
2020-06-03 04:12:51 +0000

    pushing task10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" we call that a joint """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ square class uses size """
+
+    def __init__(self, size):
+        """ instantiate with size """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ RETURN SQUARE OF SIZE """
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
