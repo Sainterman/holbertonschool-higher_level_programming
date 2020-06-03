23f047a801cf1028b169000c9fb8e301a1263e99
Santiago Castillo <sancastillog@gmail.com>
2020-06-03 23:45:20 +0000

    doc thing

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -3,6 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """  """
+    """ open file in append mode to add at end """
     with open(filename, encoding='utf-8', mode='a') as f:
         return(f.write(text))
