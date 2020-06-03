627d112d71f6e6aa99a726f455aafff19e8b33fd
Santiago Castillo <sancastillog@gmail.com>
2020-06-03 01:39:43 +0000

    pushing task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" module describes class BaseGeometry """
+
+
+class BaseGeometry():
+    """ empty class """
+
+    def __init__(self):
+        """ nothing happens yet """
+
+        pass
+
+    def area(self):
+        """ not implemented """
+
+        raise Exception("area() is not implemented")
