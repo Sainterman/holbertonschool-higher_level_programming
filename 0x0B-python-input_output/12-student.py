494f839f21459e0bc4c631130615ba9709078a7e
Santiago Castillo <sancastillog@gmail.com>
2020-06-04 02:24:19 +0000

    task 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" define class Student """
+
+
+class Student():
+    """
+    Attributes:
+        public instance:
+            first_name (string)
+            last_name (string)
+            age (int)
+    """
+
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        """ initialize """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ only attributes from attr and class """
+
+        attributes = dict()
+        if type(attrs) is list:
+            for attribute, val in self.__dict__.items():
+                if attribute in attrs:
+                    attributes[attribute] = val
+            return attributes
+        else:
+            return self.__dict__
