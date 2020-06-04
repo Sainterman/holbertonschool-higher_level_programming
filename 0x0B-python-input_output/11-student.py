98d58332bc368a767fa0144d9bbee5f229a0b94c
Santiago Castillo <sancastillog@gmail.com>
2020-06-04 01:53:45 +0000

    pushing task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" define class Student """
+
+
+class Student():
+    """ 
+    Attributes:
+        public instance:
+            first_name (string)
+            last_name (string)
+            age (int)
+    """
+
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        """ initialize """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
