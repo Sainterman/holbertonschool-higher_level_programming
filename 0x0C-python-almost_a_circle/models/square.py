79993f7c5f26383fdbdea8c46720a3045b3eaecf
Santiago Castillo <sancastillog@gmail.com>
2020-06-10 03:55:32 +0000

    up to task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,8 +19,45 @@ class Square(Rectangle):
 
     """
 
-    def __init__(id, size, x=0, y=0):
+    def __init__(self, size, x=0, y=0, id=None):
         """init with rectangle constructor
         replace height and width as size
         """
-        super().__init__(id, size, size, x, y)
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """private attr size is same as size and height"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """overwrites height and width of instance"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Info about size, polsition and id"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+            self.id, self.x, self.y, self.size)
+
+    def update(self, *args, **kwargs):
+        """
+        1st argument should be the id attr
+        2nd argument should be size attribute
+        3rd argument should be the x attribute
+        4th argument should be the y attribute
+
+        no-keywortd argument
+
+        Must check data type before updating
+        setattr not safe for different data type
+        """
+        attrs = ['id', 'size', 'x', 'y']
+        if args:
+            for i in range(len(args)):
+                setattr(self, attrs[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key in attrs:
+                    setattr(self, key, value)
