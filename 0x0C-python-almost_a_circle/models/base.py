8558dc112a8754273857fc3527474b5f0a8a7000
Santiago Castillo <sancastillog@gmail.com>
2020-06-10 04:59:54 +0000

    15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """class Base declaration"""
 
+import json
+
 
 class Base:
     """This class will be the base of all other classes in project"""
@@ -18,3 +20,9 @@ class Base:
         else:
             self.id = id
             Base.__nb_objects += 1
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if  list_dictionaries is None or list_dictionaries is []:
+            return "[]"
+        return json.dumps(list_dictionaries)
