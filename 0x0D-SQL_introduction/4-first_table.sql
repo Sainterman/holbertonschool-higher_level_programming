2f5376d1d922d296596c70b23e21f465b170265b
Santiago Castillo <sancastillog@gmail.com>
2020-07-13 22:34:28 +0000

    task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Write a script that creates a table called first_table in the current db
+-- first_table: id INT, name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
