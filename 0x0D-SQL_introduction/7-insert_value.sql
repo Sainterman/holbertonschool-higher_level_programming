60004a225f50a9d220b2731b981f0d3c87d97502
Santiago Castillo <sancastillog@gmail.com>
2020-07-14 01:27:27 +0000

    task 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- script that inserts a new row in the table first_table (database hbtn_0c_0)
+-- new row id = 89, name = holberton school
+INSERT INTO first_table(id, name) VALUES (89, "Holberton School");
