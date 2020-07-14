dae0b0d9796131306e0b1aed60db2cdb24fed1bb
Santiago Castillo <sancastillog@gmail.com>
2020-07-14 23:33:36 +0000

    task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- script that creates the MySQL server user user_0d_1.
+-- user_0d_1 should have all privileges on MySQL server, user_0d_1 password : user_0d_1_pwd
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
