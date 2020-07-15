7267a79fdd3e887d327a11d3fe573a9e4c7431c4
Santiago Castillo <sancastillog@gmail.com>
2020-07-15 00:36:58 +0000

    create user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,4 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2.
 -- user_0d_2 pwd: user_0d_2_pwd, only SELECT privilege on daabase hbtn_0d_2
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
