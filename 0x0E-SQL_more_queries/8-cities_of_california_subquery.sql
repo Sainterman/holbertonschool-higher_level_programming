85badae829f8f5d6d5bf5607fa3eb9ec3e72073f
Santiago Castillo <sancastillog@gmail.com>
2020-07-15 15:10:50 +0000

    task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,6 @@
 -- script that lists all the cities of California that can be found in the database hbtn_0d_usa.
 -- states contains one name = California, sorted ASC by cities.id, not using JOIN
-SELECT id, name FROM cities WHERE (SELECT name FROM states WHERE name IS LIKE California) ORDER BY id ASC;
+SELECT id, name FROM cities
+WHERE cities.state_id = (SELECT id FROM states
+WHERE name = 'California')
+ORDER BY id ASC;
