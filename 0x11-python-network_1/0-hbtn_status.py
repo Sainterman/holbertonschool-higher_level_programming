1bb2843423762e217c7bbd542e82a38bcb0fe285
Santiago Castillo <sancastillog@gmail.com>
2020-08-25 01:49:39 +0000

    task 0 fix xd

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,8 @@
 
 from urllib import request
 
-if __name__== "__main__":
-    with(request.urlopen("https://intranet.hbtn.io/status")) as resp:
-         data = resp.read()
-         print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-               .format(str(type(data)), str(data), str(data.decode("UTF-8"))))
+
+with(request.urlopen("https://intranet.hbtn.io/status")) as resp:
+    data = resp.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(data), data, data.decode("utf-8")))
