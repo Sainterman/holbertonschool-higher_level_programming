acaeeab8a21f4b8b1842fa6831b92017912c24a5
Santiago Castillo <sancastillog@gmail.com>
2020-08-25 02:03:30 +0000

    rtasjk 90

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """some times its hard"""
 
-from urllib import request
+import urllib.request as request
 
 
-with(request.urlopen("https://intranet.hbtn.io/status")) as resp:
+with request.urlopen("https://intranet.hbtn.io/status") as resp:
     data = resp.read()
     print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
           .format(type(data), data, data.decode("utf-8")))
