81de38994041a190e34451574f6372cc764b386c
Santiago Castillo <sancastillog@gmail.com>
2020-08-30 04:56:33 +0000

    tsk 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,6 +3,7 @@
 import sys
 import requests
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
@@ -10,3 +11,4 @@ if __name__ == "__main__":
         print("Error code: {}".format(req.status_code))
     else:
         print(req.text)
+        
