de92aa97575136663a8544442f45c53f239521f1
Santiago Castillo <sancastillog@gmail.com>
2020-08-30 06:43:07 +0000

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Check POST request with letter as parameter"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) > 0:
+        q = sys.argv[1]
+    else:
+        q = ""
+    payload = {'q': q}
+    post = requests.post(url, data=payload)
+    try:
+        r = post.json()
+        if len(r) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(r['id'], r['name']))
+    except:
+        print("Not a valid JSON")
