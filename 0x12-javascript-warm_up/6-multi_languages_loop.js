f9ae98fbb8e013a476d94f80b4c706ad6629a663
Sainterman <sancastillog@gmail.com>
2020-09-15 22:54:55 -0500

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let index = 0; index < strings.length; index++) {
+    const element = strings[index];
+    console.log(element);
+}
