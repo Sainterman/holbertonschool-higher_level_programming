7d53f879b866038c805384dc3772b83e7d41c59c
Sainterman <sancastillog@gmail.com>
2020-09-22 18:20:08 -0500

    task 0:

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,7 @@
 
 const fs = require('fs');
 
-
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-    if (err) console.log(err);
-    else console.log(data);
+  if (err) console.log(err);
+  else console.log(data);
 });
