722647d3793baa4a1e7efdc1cbf840ebd7486ef6
Sainterman <sancastillog@gmail.com>
2020-09-22 23:37:00 -0500

    task3:

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,8 +3,8 @@
 const request = require('request');
 
 const id = process.argv[2];
-request.get(`https://swapi-api.hbtn.io/api/films/${id}`, function (response, err, body) {
-  if (response.statusCode === 200) {
+request.get(`https://swapi-api.hbtn.io/api/films/${id}`, function (err, resp, body) {
+  if (resp.statusCode === 200) {
     console.log(JSON.parse(body).title);
   } else { console.log(err); }
 });
