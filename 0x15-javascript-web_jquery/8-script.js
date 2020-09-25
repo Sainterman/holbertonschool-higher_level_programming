75bedbbd249be2536da5a77f6337b14c8363f1b8
Sainterman <sancastillog@gmail.com>
2020-09-24 23:48:04 -0500

    task 9

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  const movies = data.results;
+  movies.forEach(movie => {
+    $(`<li>${movie.title}</li>`).appendTo('UL#list_movies');
+  });
+}, 'json');
